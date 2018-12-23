24a2bbb7d170912ab8ab4f6a003adf526bb2713a
ChristopherJamesN <nady.christopher@gmail.com>
2018-12-22 22:11:02 -0600

    Add logic to fetch and display slate data from an external api.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+//https://dfs-insights.herokuapp.com/slates.json
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
   }
+
+  componentDidMount() {
+      fetch('https://dfs-insights.herokuapp.com/slates.json')
+        .then(response => {
+          return response.json();
+        }).then(data => {
+          let slates = data.response.map((hit) => {
+            return(
+              <div key={hit.id}> {hit.name} </div>
+            )
+          })
+          this.setState({data: slates})
+        })
+    }
+
+    render() {
+      return ( 
+        <div>{this.state.data}</div>
+      );
+    }
 }
 
 export default App;
