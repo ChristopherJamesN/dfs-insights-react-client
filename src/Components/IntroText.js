8a529d99f40b03620b667d480fc9e519b9d430dd
ChristopherJamesN <nady.christopher@gmail.com>
2018-12-24 22:56:49 -0600

    Add jquery.

--- src/Components/IntroText.js
+++ src/Components/IntroText.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 const IntroText = () => {
     return(
       <div>
-          <h2>Welcome to DFSInsights</h2>
-          <p>This app does DFS analysis kind of.</p>
+        <Breadcrumb>
+          <BreadcrumbItem><h2>Welcome to DFSInsights</h2></BreadcrumbItem>
+        </Breadcrumb>
+        <Breadcrumb>
+          <BreadcrumbItem><p>This app does DFS analysis kind of.</p></BreadcrumbItem>
+        </Breadcrumb>
       </div>
     )
 };
