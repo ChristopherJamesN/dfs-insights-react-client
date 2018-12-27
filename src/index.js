f461ce1017906d950aeac8c9e479082ec36bf935
ChristopherJamesN <nady.christopher@gmail.com>
2018-12-26 19:46:59 -0600

    Add a players component and corresponding route.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Players from './Components/Players';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <React.Fragment>
+      <Route exact path="/" component={App} />
+      <Route exact path="/players" component={Players} />
+    </React.Fragment>
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
