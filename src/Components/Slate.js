017c657d3fbe7935c985884e328711190d8b640d
ChristopherJamesN <nady.christopher@gmail.com>
2018-12-29 21:08:19 -0600

    Update formatting on slates component.

--- src/Components/Slate.js
+++ src/Components/Slate.js
@@ -28,14 +28,28 @@ class Slate extends Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <BackButton />
-        <ul>
-          <li>ID: {this.props.match.params.id}</li>
-          <li>Max Entered Count: {this.state.data.max_entered_count}</li>
-          <li>Sport: {this.state.data.sport}</li>
-          <li>Total Entries: {this.state.data.totalEntries}</li>
-        </ul>
+        <table>
+          <tbody>
+            <tr>
+              <td>ID:</td>
+              <td>{this.props.match.params.id}</td>
+            </tr>
+            <tr>
+              <td>Max Entered Count:</td>
+              <td>{this.state.data.max_entered_count}</td>
+            </tr>
+            <tr>
+              <td>Sport:</td>
+              <td>{this.state.data.sport}</td>
+            </tr>
+            <tr>
+              <td>Total Entries:</td>
+              <td>{this.state.data.totalEntries}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
