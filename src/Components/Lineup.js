77c06dab9e4a26d96e75a4ab2fbbdaf58131ae7e
ChristopherJamesN <nady.christopher@gmail.com>
2019-03-10 11:23:58 -0500

    Update lineup compenent to display players on different lines.

--- src/Components/Lineup.js
+++ src/Components/Lineup.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 
 class Lineup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    let count = 0;
+    let lineup = this.props.Lineup.map(element => {
+      count += 1;
+      return <div key={count}>{element}</div>;
+    });
+    this.setState({ data: lineup });
+  }
+
   render() {
-    return <div>{this.props.Lineup}</div>;
+    return <div>{this.state.data}</div>;
   }
 }
 
